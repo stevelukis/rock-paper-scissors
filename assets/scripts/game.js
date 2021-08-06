380868a9523b5de20a505ecec1729d74033781d0
Steve Lukis <stevelukis88@gmail.com>
2021-08-06 20:22:07 +0800

    Implemented the functionality of comparing the superiority of two weapons

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -11,11 +11,37 @@ WEAPON_IMAGE[WEAPON.ROCK] = "assets/images/rock.png";
 WEAPON_IMAGE[WEAPON.PAPER] = "assets/images/paper.png";
 WEAPON_IMAGE[WEAPON.SCISSORS] = "assets/images/scissors.png";
 
+const RESULT = {
+  WIN: 1,
+  DRAW: 0,
+  LOSE: -1,
+}
+
+// Create table which defines whether a weapon is superior to another one or not.
+const WEAPON_SUPERIORITY_TABLE = {};
+WEAPON_SUPERIORITY_TABLE[`${WEAPON.ROCK},${WEAPON.ROCK}`] = RESULT.DRAW;
+WEAPON_SUPERIORITY_TABLE[`${WEAPON.ROCK},${WEAPON.PAPER}`] = RESULT.LOSE;
+WEAPON_SUPERIORITY_TABLE[`${WEAPON.ROCK},${WEAPON.SCISSORS}`] = RESULT.WIN;
+
+WEAPON_SUPERIORITY_TABLE[`${WEAPON.PAPER},${WEAPON.ROCK}`] = RESULT.WIN;
+WEAPON_SUPERIORITY_TABLE[`${WEAPON.PAPER},${WEAPON.PAPER}`] = RESULT.DRAW;
+WEAPON_SUPERIORITY_TABLE[`${WEAPON.PAPER},${WEAPON.SCISSORS}`] = RESULT.LOSE;
+
+WEAPON_SUPERIORITY_TABLE[`${WEAPON.SCISSORS},${WEAPON.ROCK}`] = RESULT.LOSE;
+WEAPON_SUPERIORITY_TABLE[`${WEAPON.SCISSORS},${WEAPON.PAPER}`] = RESULT.WIN;
+WEAPON_SUPERIORITY_TABLE[`${WEAPON.SCISSORS},${WEAPON.SCISSORS}`] = RESULT.DRAW;
+
 function getCPURandomWeapon() {
   return Math.floor(Math.random() * Object.keys(WEAPON).length);
+}
+
+function compareTwoWeapon(weapon1, weapon2) {
+  return WEAPON_SUPERIORITY_TABLE[`${weapon1},${weapon2}`];
 }
 
 function onWeaponClick(weapon) {
   const cpuWeapon = getCPURandomWeapon();
   cpuWeaponElem.setAttribute("src", WEAPON_IMAGE[cpuWeapon]);
+
+  console.log(compareTwoWeapon(weapon, cpuWeapon));
 }
