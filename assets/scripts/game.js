8e1b3208d0508a15abd1bcecd73c482ffd4f3a09
Steve Lukis <stevelukis88@gmail.com>
2021-08-09 12:01:09 +0800

    Randomize CPU weapon for number of times before showing the final result.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -3,9 +3,19 @@ RESULT_TEXT[RESULT.WIN] = "Congratulations! You Win!"
 RESULT_TEXT[RESULT.DRAW] = "It's a draw. Try again."
 RESULT_TEXT[RESULT.LOSE] = "Oops...You lose."
 
-function onWeaponClick(weapon) {
-  const cpuWeapon = getCPURandomWeapon();
-  cpuWeaponElem.setAttribute("src", WEAPON_IMAGE[cpuWeapon]);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function onWeaponClick(weapon) {
+  let cpuWeapon;
+
+  // Sleep in loop
+  for (let i = 0; i < 50; i++) {
+    cpuWeapon = getCPURandomWeapon();
+    cpuWeaponElem.setAttribute("src", WEAPON_IMAGE[cpuWeapon]);
+    await sleep(30);
+  }
 
   const result = compareTwoWeapon(weapon, cpuWeapon);
   resultElem.innerText = RESULT_TEXT[result];
